ac18dc672836ed07a08d92215003b4a58d8b927c
dhanshreea <DhanshreeA@users.noreply.github.com>
2024-09-26 11:27:05 +0530

    Update with new template structure

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM bentoml/model-server:0.11.0-py37
-MAINTAINER ersilia
-
-RUN pip install rdkit
-RUN pip install scikit-learn
-
-WORKDIR /repo
-COPY ./repo
--- populate.py
+++ populate.py
@@ -2,6 +2,7 @@ import sys
 import os
 import shutil
 import json
+import yaml
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 model_dir = os.path.abspath(sys.argv[1])
@@ -9,12 +10,12 @@ print("Completing for demo purposes: {0}".format(model_dir))
 
 shutil.rmtree(os.path.join(model_dir, "model"))
 shutil.copytree(os.path.join(root_dir, "model"), os.path.join(model_dir, "model"))
-shutil.copy(os.path.join(root_dir, "Dockerfile"), os.path.join(model_dir, "Dockerfile"))
+shutil.copy(os.path.join(root_dir, "install.yml"), os.path.join(model_dir, "install.yml"))
 
-metadata_json = os.path.join(os.path.join(model_dir, "metadata.json"))
+metadata_yaml = os.path.join(os.path.join(model_dir, "metadata.yml"))
 
-with open(metadata_json, "r") as f:
-    metadata = json.load(f)
+with open(metadata_yaml, "r") as f:
+    metadata = yaml.safe_load(f)
 
 metadata["Mode"] = "Retrained"
 metadata["Task"] = ["Classification"]
@@ -25,5 +26,5 @@ metadata["Output Type"] = ["Float"]
 metadata["Output Shape"] = "Single"
 metadata["Interpretation"] = "Classification score"
 
-with open(metadata_json, "w") as f:
-    json.dump(metadata, f, indent=4)
+with open(metadata_yaml, "w") as f:
+    yaml.dump(metadata, f, default_flow_style=False, sort_keys=False)
